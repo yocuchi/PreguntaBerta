982b4e5495bc1487f670753d74331d59a1392997
Fhcuchi <hernandezcfran@madrid.es>
2025-11-21 15:39:56 +0100

    juego tablas multiplicar

---
+++ games/2Pri-TablasMultiplicar.js
@@ -0,0 +1,41 @@
+const preguntasJSON = [
+    // Tabla del 2
+    { nombre: "2 x 1", respuesta: "2" },
+    { nombre: "2 x 2", respuesta: "4" },
+    { nombre: "2 x 3", respuesta: "6" },
+    { nombre: "2 x 4", respuesta: "8" },
+    { nombre: "2 x 5", respuesta: "10" },
+    { nombre: "2 x 6", respuesta: "12" },
+    { nombre: "2 x 7", respuesta: "14" },
+    { nombre: "2 x 8", respuesta: "16" },
+    { nombre: "2 x 9", respuesta: "18" },
+    { nombre: "2 x 10", respuesta: "20" },
+    // Tabla del 5
+    { nombre: "5 x 1", respuesta: "5" },
+    { nombre: "5 x 2", respuesta: "10" },
+    { nombre: "5 x 3", respuesta: "15" },
+    { nombre: "5 x 4", respuesta: "20" },
+    { nombre: "5 x 5", respuesta: "25" },
+    { nombre: "5 x 6", respuesta: "30" },
+    { nombre: "5 x 7", respuesta: "35" },
+    { nombre: "5 x 8", respuesta: "40" },
+    { nombre: "5 x 9", respuesta: "45" },
+    { nombre: "5 x 10", respuesta: "50" },
+    // Tabla del 10
+    { nombre: "10 x 1", respuesta: "10" },
+    { nombre: "10 x 2", respuesta: "20" },
+    { nombre: "10 x 3", respuesta: "30" },
+    { nombre: "10 x 4", respuesta: "40" },
+    { nombre: "10 x 5", respuesta: "50" },
+    { nombre: "10 x 6", respuesta: "60" },
+    { nombre: "10 x 7", respuesta: "70" },
+    { nombre: "10 x 8", respuesta: "80" },
+    { nombre: "10 x 9", respuesta: "90" },
+    { nombre: "10 x 10", respuesta: "100" }
+];
+const tituloJuego = "Tablas de Multiplicar"; // Título del juego
+const prefijoPregunta = "¿Cuánto es "; // Prefijo de la pregunta
+const sufijoPregunta = " ?"; // Sufijo de la pregunta
+const curso = "2ºPrimaria";
+const descripcion = "Preguntas sobre las tablas de multiplicar del 2, 5 y 10";
+const tipoJuego = "preguntas"; // Puede ser "preguntas" o "tablaperiodica"
